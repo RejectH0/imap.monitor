c6e5da3e89070c3a55d77923eea0515177f1e617
Gregg <gregg@rejecth0.com>
2024-01-27 19:50:27 -0700

    Changed listener port to 8082 to avoid conflict

--- app/app.py
+++ app/app.py
@@ -60,4 +60,4 @@ def home():
     return render_template('index.html', header_title='Main Menu: IMAP Monitor')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8082)
